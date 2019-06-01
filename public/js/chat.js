38f25a6f0da1dc3013dcb7dd5abcdd37b3f6c8d9
deliadominte <deliadominte@yahoo.com>
2019-06-01 12:58:44 +0300

    chat 1.0

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,68 @@
+
+// Make connection
+var socket = io.connect('http://localhost:5500');
+
+// Query DOM
+var message = document.getElementById('message'),
+      handle,isClient,
+      btn = document.getElementById('send'),
+      output = document.getElementById('output'),
+      feedback = document.getElementById('feedback');
+const userId = Cookies.get('userId');
+db.collection('users').doc(userId).get().then(doc => {
+        if (doc.exists) {
+          const user = doc.data();
+          handle = user.username;
+          isClient=true;
+          
+          console.log(isClient);
+        }});
+db.collection('nutritionists').doc(userId).get().then(doc => {
+          if (doc.exists) {
+            const user = doc.data();
+            handle = user.username;
+            isClient=false;
+            
+            console.log(isClient);
+          }});
+// Emit events
+btn.addEventListener('click', function(){
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle
+    });
+    message.value = "";
+});
+
+message.addEventListener('keypress', function(){
+    socket.emit('typing', handle);
+})
+
+// Listen for events
+socket.on('chat', function(data){
+    feedback.innerHTML = '';
+    let objToday = new Date(), curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()),
+    curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes();
+    if(isClient==false)
+    output.innerHTML += '\
+    <div class="w3-container w3-card w3-white w3-round w3-margin"><br>\
+            <img src="./media/avatar_doctor.png" alt="Avatar" class="w3-left w3-circle w3-margin-right" style="width:60px">\
+            <span class="w3-right w3-opacity w3-margin-bottom">'+curHour+':'+curMinute+'</span><br>\
+            <p><strong>' + data.handle + ': </strong>' + data.message + '</p>\
+            </div>  \
+    ';
+    else output.innerHTML += '\
+    <div class="w3-container w3-card w3-white w3-round w3-margin"><br>\
+    <img src="./media/avatar.png" alt="Avatar" class="w3-right w3-circle w3-margin-left" style="width:60px">\
+    <span class="w3-left w3-opacity w3-margin-bottom">'+curHour+':'+curMinute+'</span><br>\
+            <p><strong>' + data.handle + ': </strong>' + data.message + '</p>\
+            </div>  \
+    ';
+});
+
+socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+});
 function openForm(i) {
   if (i==1)
     document.getElementById("myForm").style.display = "block";
